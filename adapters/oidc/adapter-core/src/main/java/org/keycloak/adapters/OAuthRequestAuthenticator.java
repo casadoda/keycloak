39244f338198547c052e95bd48fc0eb3080b4d0c
dcasado <horspiste@gmail.com>
2018-12-13 18:43:34 +0100

    Patch for Weblogic.
    Removal of default port 443 when https is used in Redirect_URI.

--- adapters/oidc/adapter-core/src/main/java/org/keycloak/adapters/OAuthRequestAuthenticator.java
+++ adapters/oidc/adapter-core/src/main/java/org/keycloak/adapters/OAuthRequestAuthenticator.java
@@ -38,6 +38,7 @@ import org.keycloak.util.TokenUtil;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Map;
 import java.util.logging.Level;
@@ -139,9 +140,55 @@ public class OAuthRequestAuthenticator {
     protected String getCode() {
         return getQueryParamValue(OAuth2Constants.CODE);
     }
+    
+    /*
+	 * ^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?
+   12            3  4          5       6  7        8 9
+The numbers in the second line above are only to assist readability; they indicate the reference points for each subexpression (i.e., each paired parenthesis). We refer to the value matched for subexpression as $. For example, matching the above expression to
+
+  http://www.ics.uci.edu/pub/ietf/uri/#Related
+results in the following subexpression matches:
+
+  $1 = http:
+  $2 = http
+  $3 = //www.ics.uci.edu
+  $4 = www.ics.uci.edu
+  $5 = /pub/ietf/uri/
+  $6 = <undefined>
+  $7 = <undefined>
+  $8 = #Related
+  $9 = Related
+	 */
+	public String patchUriWeblogic(String url)
+	{
+		try {
+			URI reqURL = new URI(url);
+			
+			if (reqURL.getScheme().equalsIgnoreCase("https") && reqURL.getPort()==443)
+			{
+				StringBuilder newURL = new StringBuilder(); 
+				//Schema + Host (without port) + patch
+				newURL.append(reqURL.getScheme()+"://"+reqURL.getHost()+reqURL.getPath());
+				//+query if exists
+				if (reqURL.getRawQuery()!=null)
+					newURL.append("?"+reqURL.getRawQuery());
+				//+fragment if exists
+				if (reqURL.getRawFragment()!=null)
+					newURL.append("#"+reqURL.getRawFragment());
+				return newURL.toString();
+
+			} else 
+				return url;
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			return url;
+		}
+	}
 
     protected String getRedirectUri(String state) {
         String url = getRequestUrl();
+        url = patchUriWeblogic(url);
+        
         log.debugf("callback uri: %s", url);
       
         if (!facade.getRequest().isSecure() && deployment.getSslRequired().isRequired(facade.getRequest().getRemoteAddr())) {
